ecd7cfa90cd847d693c44a1da143850d003d08b5
Digerati <cabal@digerati.design>
2025-01-08 18:36:56 +0000

    Support adding leaflet provider

--- src/digerati/openStreetMap.ts
+++ src/digerati/openStreetMap.ts
@@ -12,34 +12,8 @@ export const OpenStreetMapManager = () => {
      * @param message  String
      */
     const log = (message) => {
-        console.log(message);
+        // console.log(message);
     };
-    /**
-     * Get Satellite Tile Layer.
-     * 
-     * @returns 
-     */
-    const getSatelliteTileLayer = () => L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-        maxZoom: 18,
-    });
-    /**
-     * Get Street Map Tile Layer.
-     * 
-     * @returns 
-     */
-    const getStreetMapTileLayer = () => L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-        attribution: 'Map data &copy; <a href="https://openstreetmap.org" target="_blank">OpenStreetMap</a>',
-        maxZoom: 18,
-    });
-    /**
-     * Get Roads and Labels Tile Layer.
-     * @returns 
-     */
-    const getRoadsAndLabelsTileLayer = () => L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/roads_and_labels/{z}/{x}/{y}.png', {
-        attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        maxZoom: 18,
-    });
     /**
      * Get Map Instance.
      * 
@@ -99,13 +73,10 @@ export const OpenStreetMapManager = () => {
      * @param locations 
      * @param markerGroup 
      * @param openStreetMap 
-     * @param satellite 
-     * @param streetMap 
-     * @param roadsAndLabels 
-     * @param defaultType 
+     * @param leafletProviderUrl 
      * @param zoomLevel 
      */
-    const displayMap = (locations, markerGroup, openStreetMap, satellite, streetMap, roadsAndLabels, defaultType, zoomLevel) => {
+    const displayMap = (locations, markerGroup, openStreetMap, leafletProviderUrl, zoomLevel) => {
         log('Display OpenStreetMap');
         if (locations.length > 1 && markerGroup.getBounds().isValid()) {
             openStreetMap.fitBounds(markerGroup.getBounds(), { padding: [50, 50] });
@@ -115,81 +86,11 @@ export const OpenStreetMapManager = () => {
                 zoomLevel
             );
         }
-        switch (defaultType) {
-            case 'satellite':
-                openStreetMap.addLayer(satellite);
-                break;
-            case 'streetMap':
-                openStreetMap.addLayer(streetMap);
-                break;
-            case 'roadsAndLabels':
-                openStreetMap.addLayer(satellite);
-                openStreetMap.addLayer(roadsAndLabels);
-                break;
-        }
-    };
-    /**
-     * Generate Type Selector.
-     * 
-     * @param mapId 
-     * @param openStreetMap 
-     * @param satellite 
-     * @param streetMap 
-     * @param roadsAndLabels 
-     * @param defaultType 
-     */
-    const generateTypeSelector = (mapId, openStreetMap, satellite, streetMap, roadsAndLabels, defaultType) => {
-        log('Generate OpenStreetMap Type Selector');
-        const container = document.createElement('ul');
-        container.className = 'openstreetmap-type-selector';
-        const types = [
-            {
-                label: 'Map',
-                value: 'streetMap'
-            },
-            {
-                label: 'Satellite',
-                value: 'satellite'
-            },
-        ];
-        types.forEach((type) => {
-            const listItem = document.createElement('li');
-            const label = document.createElement('label');
-            const input = document.createElement('input');
-            const span = document.createElement('span');
-            input.type = 'radio';
-            input.value = type.value;
-            input.name = 'openstreetmap-type';
-            if (defaultType === type.value) {
-                input.checked = true;
-            }
-            input.addEventListener('change', () => {
-                log(`OpenStreetMap Type Selector [${type.value}]`);
-                switch (type.value) {
-                    case 'streetMap':
-                        openStreetMap.removeLayer(satellite);
-                        openStreetMap.removeLayer(roadsAndLabels);
-                        openStreetMap.addLayer(streetMap);
-                        break;
-                    case 'satellite':
-                        openStreetMap.removeLayer(streetMap);
-                        openStreetMap.removeLayer(roadsAndLabels);
-                        openStreetMap.addLayer(satellite);
-                        break;
-                    case 'roadsAndLabels':
-                        openStreetMap.removeLayer(streetMap);
-                        openStreetMap.addLayer(satellite);
-                        openStreetMap.addLayer(roadsAndLabels);
-                        break;
-                }
-            });
-            span.textContent = type.label;
-            label.appendChild(input);
-            label.appendChild(span);
-            listItem.appendChild(label);
-            container.appendChild(listItem);
-        });
-        document.getElementById(mapId).appendChild(container);
+        openStreetMap.addLayer(L.tileLayer(leafletProviderUrl, {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            minZoom: 0,
+            maxZoom: 20,
+        }));
     };
 
     /**
@@ -197,22 +98,22 @@ export const OpenStreetMapManager = () => {
      * 
      * @param mapId
      * @param locations 
-     * @param defaultType 
+     * @param leafletProviderUrl
      * @param zoomLevel 
      * @param mapMarkerUrl 
      * @returns 
      */
-    const initialiseOpenStreetMap = (mapId, locations, defaultType, zoomLevel, mapMarkerUrl) => {
+    const initialiseOpenStreetMap = (mapId, locations, leafletProviderUrl, zoomLevel, mapMarkerUrl) => {
         return new Promise((resolve) => {
-            log(`Initialising OpenStreetMap (${mapId}), [${defaultType}]`);
-            const satellite = getSatelliteTileLayer();
-            const streetMap = getStreetMapTileLayer();
-            const roadsAndLabels = getRoadsAndLabelsTileLayer();
+            log(`Initialising OpenStreetMap (${mapId})]`);
             const openStreetMap = getMapInstance(mapId);
             const markers = getMarkers(openStreetMap, locations, mapMarkerUrl);
             const markerGroup = L.featureGroup(markers).addTo(openStreetMap);
-            displayMap(locations, markerGroup, openStreetMap, satellite, streetMap, roadsAndLabels, defaultType, zoomLevel);
-            generateTypeSelector(mapId, openStreetMap, satellite, streetMap, roadsAndLabels, defaultType);
+            log(openStreetMap);
+            log(markers);
+            log(leafletProviderUrl);
+            log(markerGroup);
+            displayMap(locations, markerGroup, openStreetMap, leafletProviderUrl, zoomLevel);
             resolve();
         });
     };
--- src/goodDogVeterinaryCare/openStreetMap.ts
+++ src/goodDogVeterinaryCare/openStreetMap.ts
@@ -11,11 +11,14 @@ export const openStreetMap = () => {
         return;
     }
     target.forEach((map) => {
-        const marker = map.querySelector(['[dd-map="marker"]']);
+        const mapId = map.getAttribute('id');
         const locations = map.querySelectorAll(['[dd-map="location"]']);
-        if (!marker || !locations) {
+        const leafletProvider = map.querySelector(['[dd-leaflet-provider]']);
+        const marker = map.querySelector(['[dd-map="marker"]']);
+        if (!locations || !leafletProvider || !marker) {
             return;
         }
+        const leafletProviderUrl = leafletProvider.getAttribute('dd-leaflet-provider');
         const mapMarkers = [];
         marker.remove();
         locations.forEach((location) => {
@@ -36,9 +39,9 @@ export const openStreetMap = () => {
         });
         const mapManager = OpenStreetMapManager();
         mapManager.initialiseOpenStreetMap(
-            'map-container',
+            mapId,
             mapMarkers,
-            'streetMap',
+            leafletProviderUrl,
             12,
             'https://cdn.prod.website-files.com/67606dd3484cbf8b6d8ecb18/677eaebf644dd758036e8b73_map-marker.svg'
         ).then(() => {
