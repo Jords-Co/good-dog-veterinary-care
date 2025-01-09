16b1bd27270b67e0861fd25f22eac1fe24673ed9
Digerati <cabal@digerati.design>
2025-01-09 19:49:45 +0000

    Fix Team Member modal

---
+++ src/digerati/teamMemberModal.ts
@@ -0,0 +1,29 @@
+/**
+ * Team Member Modal.
+ * 
+ * Dynamically populate Team Member Modal
+ * with CMS Collection Item Properties
+ * from corresponding Team Member Item.
+ * 
+ * @author <cabal@digerati.design>
+ */
+export const teamMemberModal = () => {
+    const showModalLinks = document.querySelectorAll('[dd-modal="show"]'),
+        hideModalLinks = document.querySelectorAll('[dd-modal="hide"]'),
+        modal = document.querySelector('[dd-modal="modal"]');
+    if (!showModalLinks || !hideModalLinks || !modal) {
+        return;
+    }
+    showModalLinks.forEach((showModalLink) => {
+        showModalLink.addEventListener('click', () => {
+            let teamMember = showModalLink.parentElement?.parentElement?.parentElement;
+            if (!teamMember) {
+                return false;
+            }
+            modal.querySelector(['[dd-modal="image"]']).src = teamMember.querySelector('[dd-tm="image"]').src;
+            modal.querySelector(['[dd-modal="name"]']).innerText = teamMember.querySelector('[dd-tm="name"]').innerText;
+            modal.querySelector(['[dd-modal="job-title"]']).innerText = teamMember.querySelector('[dd-tm="job-title"').innerText;
+            modal.querySelector(['[dd-modal="biography"]']).innerText = teamMember.querySelector('[dd-tm="biography"]').innerText;
+        }, { passive: true });
+    });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { skipToMainContent } from "$digerati/skipToMainContent";
 import { currentYear } from "$digerati/currentYear";
 import { openStreetMap } from "$goodDogVeterinaryCare/openStreetMap";
+import { teamMemberModal } from "$digerati/teamMemberModal";
 
 window.Webflow || [];
 window.Webflow.push(() => {
   skipToMainContent();
   currentYear();
   openStreetMap();
+  teamMemberModal();
 });
